4aea53143ae4967e1094421488341e17258d6035
abaraba <holycl@dingtalk.com>
2023-01-04 20:53:34 +0800

    first

---
+++ AES/src/AES.jl
@@ -0,0 +1,27 @@
+module AES
+
+	using StaticArrays, Random
+
+	abstract type AbstractSymmetricKey end
+	abstract type AbstractCipher end
+	abstract type AbstractCipherCache end
+
+	abstract type AbstractAESKey <: AbstractSymmetricKey end
+	abstract type AbstractAESCache <: AbstractCipherCache end
+
+	include("constants.jl")
+	include("types.jl")
+	include("block_encryption.jl")
+	include("block_decryption.jl")
+	include("modes/cbc.jl")
+	include("modes/ctr.jl")
+	include("modes/ecb.jl")
+	include("encrypt.jl")
+	include("decrypt.jl")
+
+	export AESCipher
+	export AES128Key, AES192Key, AES256Key
+	export AESCache
+	export encrypt, decrypt
+
+end # module
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # ncmdump
+---
+
+## Overview
+将网易云会员下载的.ncm音乐文件转换为.mp3或.flac文件
+
+
+## Required
+	julia
+
+## Installation
+
+```julia
+julia init.jl
+```
+
+(optional) AES
+
+## Getting started
+- Linux
+    - ```bash
+        bash dump.sh /path/folder/
+        # or
+        bash dump.sh music1.ncm music2.ncm
+        ```
