e1f2d439bc32651d36e558a0b706ecdda77e1286
abaraba <holycl@dingtalk.com>
2023-01-05 18:46:39 +0800

    v2

--- init.jl
+++ init.jl
@@ -1,10 +1,12 @@
 ENV["JULIA_PKG_SERVER"] = "https://mirrors.ustc.edu.cn/julia"
 using Pkg
 Pkg.activate(".")
-tmppath="./AES"
-if isdir(tmppath)
-    Pkg.develop(path=tmppath)
-else
-    Pkg.add("AES")
-end
-Pkg.add("JSON3")
+# tmppath="./AES"
+# if isdir(tmppath)
+#     Pkg.develop(path=tmppath)
+# else
+#     Pkg.add("AES")
+# end
+# Pkg.add("JSON3")
+
+Pkg.develop(path="./NcmDump")
