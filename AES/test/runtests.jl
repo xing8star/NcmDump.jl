9fead6af7a17824a53cb65477f55159515c1d5d0
abaraba <holycl@dingtalk.com>
2023-04-02 13:19:20 +0800

    remove AES

--- AES/test/runtests.jl
+++
@@ -1,9 +0,0 @@
-using SafeTestsets
-
-@time begin
-	@time @safetestset "Interface Tests" begin include("interface.jl") end
-	@time @safetestset "Block Encryption/Decryption tests" begin include("blocktest.jl") end
-	@time @safetestset "CBC Mode tests" begin include("cbc.jl") end
-	@time @safetestset "CTR Mode tests" begin include("ctr.jl") end
-	@time @safetestset "ECB Mode tests" begin include("ecb.jl") end
-end
